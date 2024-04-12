7a2ea9d8671c0d08d77a3671e62f396617ec1079
Michal Stefanak <stefanak.michal@gmail.com>
2024-04-12 09:26:24 +0200

    test room join and listen to event

--- e2e/index.spec.ts
+++ e2e/index.spec.ts
@@ -1,4 +1,5 @@
 import { test, expect } from '@playwright/test';
+import EventType from "../src/EventType";
 
 test('perform ThingsDB test', async ({ page }) => {
     await page.goto('http://localhost:9000/');
@@ -17,4 +18,17 @@ test('perform ThingsDB test', async ({ page }) => {
 
     const hello = await page.evaluate(() => window["thingsdb"].query('@thingsdb', '"Hello World!"'));
     expect(hello).toBe('Hello World!');
+
+    const roomId = await page.evaluate(() => window["thingsdb"].query('@:stuff', `if (.has("test_room")) .del("test_room");
+.test_room = room();
+.test_room.id();`));
+    expect(roomId).toBeGreaterThan(0);
+
+    const watchDog = page.waitForFunction(() => window["joined"], null, {timeout: 5});
+    await page.evaluate((r) => {
+        window["joined"] = false;
+        window["thingsdb"].addEventListener((type: number, message: any): boolean => { window["joined"] = type === EventType.ON_JOIN; });
+        return window["thingsdb"].join('@:stuff', r);
+    }, roomId);
+    await watchDog;
 });
